c587fb8636c02e7652c10e737d2b8136786f442e
Tyuiu.BrylinaVV.outlook.com <brylina2008@yandex.ru>
2026-09-08 18:56:33 +0500

    Task1

--- Tyuiu.BrylinaVV.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.BrylinaVV.Sprint0.Task1.V0/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            Console.WriteLine("Hello, World!");
             string name = "Вероника";
             int age = 17;
             Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет.");
