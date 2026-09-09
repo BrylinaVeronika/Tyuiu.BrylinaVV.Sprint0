734cca2be312af7f7a14badbd24b381ecf66d506
Tyuiu.BrylinaVV.outlook.com <brylina2008@yandex.ru>
2026-09-09 08:52:51 +0500

    Task2

---
+++ Tyuiu.BrylinaVV.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BrylinaVV.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.BrylinaVV.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Вероника";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Вероника", res);
+        }
+    }
+}
