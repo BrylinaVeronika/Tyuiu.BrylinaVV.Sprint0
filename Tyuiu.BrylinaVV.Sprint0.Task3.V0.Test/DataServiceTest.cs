9a766fd2abe624170f95d9fdbf1f79e345554c75
Tyuiu.BrylinaVV.outlook.com <brylina2008@yandex.ru>
2026-09-09 09:24:13 +0500

    Task3

--- Tyuiu.BrylinaVV.Sprint0.Task3.V0.Test/DataServiceTest.cs
+++ Tyuiu.BrylinaVV.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.BrylinaVV.Sprint0.Task3.V0.Test
     public sealed class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CheckedValid()
         {
             Assert.AreEqual(10, DataService.Sum(5, 5));
         }
